a65fd006a606970beec6cc546a4f3ef601c453ad
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-09-15 12:29:43 -0600

    refactor: routes to be more restful.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v1 do
       resources :customers do 
-        resources :subscriptions, only: %i[index]
+        resources :subscriptions, only: %i[index create]
         delete '/subscriptions/:id', to: 'subscriptions#cancel'
       end
-      post '/customers/:customer_id/teas/:tea_id', to: 'subscriptions#create'
     end
   end
 end
