e2ed4897900c2df7cb8bc55ca0261a5928211ae0
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-09-14 19:10:28 -0600

    fix: typos in test data.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -106,9 +106,9 @@ def test_data
 
   @subscription_1 = Subscription.create!(title: "montlhy saver", price: 9.99, status: "active", frequency: "monthly", tea_units: 30, tea_unit_size: 12, customer: @customer_1, tea: @tea_1 )
   @subscription_2 = Subscription.create!(title: "half saver", price: 15.99, status: "active", frequency: "bi-weekly", tea_units: 15, tea_unit_size: 16, customer: @customer_2, tea: @tea_2 )
-  @subscription_3 = Subscription.create!(title: "montlhy saver", price: 5.99, status: "cancelled", frequency: "quarterly", tea_units: 90, tea_unit_size: 12, tea: @tea_1 )
-  @subscription_4 = Subscription.create!(title: "flat deal saver", price: 4.99, status: "cancelled", frequency: "annual", tea_units: 730, tea_unit_size: 16 )
-  @subscription_5 = Subscription.create!(title: "flat deal saver", price: 4.99, status: "cancelled", frequency: "annual", tea_units: 730, tea_unit_size: 16, customer: customer_1 )
-  @subscription_6 = Subscription.create!(title: "flat deal saver", price: 4.99, status: "cancelled", frequency: "annual", tea_units: 730, tea_unit_size: 16, tea: tea_1 )
+  @subscription_3 = Subscription.create!(title: "montlhy saver", price: 5.99, status: "cancelled", frequency: "quarterly", tea_units: 90, tea_unit_size: 12, customer: @customer_1, tea: @tea_2 )
+  @subscription_4 = Subscription.create!(title: "flat deal saver", price: 4.99, status: "cancelled", frequency: "annual", tea_units: 730, tea_unit_size: 16, customer: @customer_2, tea: @tea_1 )
+  @subscription_5 = Subscription.create!(title: "flat deal saver", price: 4.99, status: "cancelled", frequency: "annual", tea_units: 730, tea_unit_size: 16, customer: @customer_1 )
+  @subscription_6 = Subscription.create!(title: "flat deal saver", price: 4.99, status: "cancelled", frequency: "annual", tea_units: 730, tea_unit_size: 16, tea: @tea_1 )
   @subscription_7 = Subscription.create!(title: "flat deal saver", price: 4.99, status: "cancelled", frequency: "annual", tea_units: 730, tea_unit_size: 16 )
 end
