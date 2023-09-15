75994aeb0c8bf4adb91acf518aa5dfda6215d68d
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-09-15 12:29:32 -0600

    refactor: error render methods.

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,85 +1,74 @@
 class Api::V1::SubscriptionsController < ApplicationController
-  before_action :find_customer
+  before_action :find_customer, only: [:index, :create, :cancel]
+  before_action :find_tea, only: [:create]
+  before_action :find_subscription, only: [:create, :cancel]
+
+  ACTIVE_STATUS = 'active'.freeze
+  CANCELLED_STATUS = 'cancelled'.freeze
 
   def index
-      subscriptions = @customer.subscriptions
-      render json: SubscriptionSerializer.new(subscriptions), status: 200
+    subscriptions = @customer.subscriptions
+    render json: SubscriptionSerializer.new(subscriptions), status: 200
   end
-  
+
   def create
-    find_tea
-    if @tea.nil?
-      render json: { error: 'Tea not found' }, status: :not_found
-    else
-      subscription = Subscription.find_by(customer_id: @customer.id, tea_id: @tea.id)
-      if subscription 
-        if subscription.status == "active"
-          render json: { error: "Customer already has an active subscription for #{@tea.title}"}, status: :unprocessable_entity
-        elsif subscription.status == "cancelled"
-          subscription.update!(status: "active")
-          render json: SubscriptionSerializer.new(subscription), status: 200
-        end
-      else
-        created_subscription = Subscription.create!(subscription_params)
-        created_subscription.update!(status: "active")
-        render json: SubscriptionSerializer.new(created_subscription), status: :created
-      end
-    end
+    return handle_existing_subscription(@subscription) if @subscription
+    created_subscription = Subscription.create!(subscription_params.merge(status: ACTIVE_STATUS))
+    render_subscription(created_subscription, :created)
   end
 
   def cancel
-    @subscription = Subscription.find_by(id: params[:id])
     if @subscription
-      @subscription.update(status: 'cancelled')
+      @subscription.update(status: CANCELLED_STATUS)
       render json: { message: 'Subscription successfully cancelled' }, status: :ok
     else
       render json: { error: 'Subscription not found' }, status: :not_found
     end
   end
 
-  # def update
-  #   find_subscription
-  #   if @subscription.status == "cancelled"
-  #     @subscription.update!(status: "active")
-  #   end
-  #   require 'pry'; binding.pry
-  # end
-
   private
+
   def find_customer
     @customer = Customer.find_by(id: params[:customer_id])
-    unless @customer
-      render json: { error: 'Customer not found' }, status: :not_found
-      return
-    end
+    render_customer_not_found unless @customer
   end
 
   def find_tea
-    # require 'pry'; binding.pry
     @tea = Tea.find_by(id: params[:tea_id])
-    # unless @tea
-    #   render json: { error: 'Tea not found' }, status: :not_found
-    #   return
-    # end
+    render_tea_not_found unless @tea
   end
 
-  # def find_subscription
-  #   @customer = Customer.find_by(id: params[:customer_id])
-  #   @tea = Tea.find_by(id: params[:tea_id])
-  #   @subscription = Subscription.find_by(id: params[:subscription_id])
+  def find_subscription
+    @subscription = Subscription.find_by(customer_id: params[:customer_id], tea_id: params[:tea_id])
+    if @subscription
+      @subscription
+    else
+      @subscription = Subscription.find_by(id: params[:id], customer_id: params[:customer_id])
+    end
+  end
 
-  #   if @subscription
-  #     @subscription
-  #   else
-  #     @subscription = Subscription.find_by(customer_id: @customer.id, tea_id: @tea.id)
-  #     unless @subscription
-  #       render json: { error: 'Subscription not found' }, status: :not_found
-  #       return
-  #     end
-  #   end
-  # end
+  def handle_existing_subscription(subscription)
+    if subscription.status == ACTIVE_STATUS
+      render json: { error: "Customer already has an active subscription for #{subscription.tea.title}" }, status: :unprocessable_entity
+    elsif subscription.status == CANCELLED_STATUS
+      subscription.update!(status: ACTIVE_STATUS)
+      render_subscription(subscription, :ok)
+    end
+  end
+
+  def render_subscription(subscription, status)
+    render json: SubscriptionSerializer.new(subscription), status: status
+  end
+
+  def render_customer_not_found
+    render json: { error: 'Customer not found' }, status: :not_found
+  end
+
+  def render_tea_not_found
+    render json: { error: 'Tea not found' }, status: :not_found
+  end
 
   def subscription_params
-    params.permit(:title, :price, :frequency, :tea_units, :tea_unit_size, :customer_id, :tea_id )
+    params.permit(:title, :price, :frequency, :tea_units, :status, :tea_unit_size, :customer_id, :tea_id)
   end
 end
