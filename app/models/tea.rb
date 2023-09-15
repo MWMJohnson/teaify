33103732060c1a8445d9e7efc070a8455f597902
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-09-15 00:53:25 -0600

    edit: re-order validations.

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -2,8 +2,9 @@ class Tea < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :temperature, presence: true
-  validates :temperature, numericality: true
   validates :brew_time_seconds, presence: true
+  
+  validates :temperature, numericality: true
   validates :brew_time_seconds, numericality: true
 
   has_many :subscriptions, dependent: :destroy
